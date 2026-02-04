ccab477300af4e6098fc6659e785808348069500
Tim <hungtamyie@gmail.com>
2026-02-03 21:50:15 -0600

    test

--- Main/camera_2d.gd
+++ Main/camera_2d.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Camera2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,9 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print("hello world")
-	print("invincible change")
-	print("invincible change")
-	print("a")
-	print("Sniper change")
 	pass
