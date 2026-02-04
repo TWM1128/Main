975478dfe18c7ea5cbb648d7801697bdd8538732
Taylor <taylormccafferty@icloud.com>
2026-02-03 23:16:03 -0500

    bugged

--- Character/character.gd
+++ Character/character.gd
@@ -2,8 +2,12 @@ extends CharacterBody2D
 var speed:float=300
 func _physics_process(delta: float) -> void:
 	if(Input.is_action_pressed("move left")):
-		position.x=position.x-speed*delta
+		velocity.x=-speed
 	elif(Input.is_action_pressed("move right")):
-		position.x=position.x+speed*delta
+		velocity.x=speed
+	else:
+		velocity.x=0
+	print(position.x)
+	move_and_slide()
 	position.x=clampf(position.x,0,1152)
 	pass
